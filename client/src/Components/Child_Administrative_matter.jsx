63420d2ec684c75c0d263fa93b62ceb91a426b29
Gaurav Gautam <gautamgaurav945@gmail.com>
2023-11-02 00:58:53 +0530

    added new designs

--- client/src/Components/Child_Administrative_matter.jsx
+++ client/src/Components/Child_Administrative_matter.jsx
@@ -1,173 +1,137 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef } from "react";
 
 const Child_Administrative_matter = (props) => {
+  const sidebarRightText = useRef(null);
+  const sidebarRightLinks = useRef(null);
+  const sidebarRightImages = useRef(null);
+  const sidebarRightVideos = useRef(null);
 
-const sidebarRightText=useRef(null);
-const sidebarRightLinks=useRef(null);
-const sidebarRightImages=useRef(null);
-const sidebarRightVideos=useRef(null);
+  const arrowDown = useRef(null);
+  const downData = useRef(null);
 
-
-const arrowDown=useRef(null);
-const downData=useRef(null);
-
-
-
-useEffect(()=>{
-
-if(sidebarRightText.current && sidebarRightLinks.current &&
-  sidebarRightImages.current &&  sidebarRightVideos.current ){
-    sidebarRightText.current.onclick=()=>{
-      sidebarRightText.current.style.backgroundColor="#1937be";
-      sidebarRightText.current.style.color="white";
-      sidebarRightLinks.current.style.backgroundColor="white";
-      sidebarRightLinks.current.style.color="black";
-      sidebarRightImages.current.style.backgroundColor="white";
-      sidebarRightImages.current.style.color="black";
-      sidebarRightVideos.current.style.backgroundColor="white"
-      sidebarRightVideos.current.style.color="black"
-
-     
-    }
-    sidebarRightLinks.current.onclick=()=>{
-      sidebarRightText.current.style.backgroundColor="white";
-      sidebarRightText.current.style.color="black";
-      sidebarRightLinks.current.style.backgroundColor="#1937be";
-      sidebarRightLinks.current.style.color="white";
-      sidebarRightImages.current.style.backgroundColor="white";
-      sidebarRightImages.current.style.color="black";
-      sidebarRightVideos.current.style.backgroundColor="white"
-      sidebarRightVideos.current.style.color="black"
-
-    }
-    sidebarRightImages.current.onclick=()=>{
-      sidebarRightText.current.style.backgroundColor="white";
-      sidebarRightText.current.style.color="black";
-      sidebarRightLinks.current.style.backgroundColor="white";
-      sidebarRightLinks.current.style.color="black";
-      sidebarRightImages.current.style.backgroundColor="#1937be";
-      sidebarRightImages.current.style.color="white";
-      sidebarRightVideos.current.style.backgroundColor="white"
-      sidebarRightVideos.current.style.color="black"
-
-    }
-    sidebarRightVideos.current.onclick=()=>{
-      sidebarRightText.current.style.backgroundColor="white";
-      sidebarRightText.current.style.color="black";
-      sidebarRightLinks.current.style.backgroundColor="white";
-      sidebarRightLinks.current.style.color="black";
-      sidebarRightImages.current.style.backgroundColor="white";
-      sidebarRightImages.current.style.color="black";
-      sidebarRightVideos.current.style.backgroundColor="#1937be"
-      sidebarRightVideos.current.style.color="white"
-
+  useEffect(() => {
+    if (
+      sidebarRightText.current &&
+      sidebarRightLinks.current &&
+      sidebarRightImages.current &&
+      sidebarRightVideos.current
+    ) {
+      sidebarRightText.current.onclick = () => {
+        sidebarRightText.current.style.backgroundColor = "#1937be";
+        sidebarRightText.current.style.color = "white";
+        sidebarRightLinks.current.style.backgroundColor = "white";
+        sidebarRightLinks.current.style.color = "black";
+        sidebarRightImages.current.style.backgroundColor = "white";
+        sidebarRightImages.current.style.color = "black";
+        sidebarRightVideos.current.style.backgroundColor = "white";
+        sidebarRightVideos.current.style.color = "black";
+      };
+      sidebarRightLinks.current.onclick = () => {
+        sidebarRightText.current.style.backgroundColor = "white";
+        sidebarRightText.current.style.color = "black";
+        sidebarRightLinks.current.style.backgroundColor = "#1937be";
+        sidebarRightLinks.current.style.color = "white";
+        sidebarRightImages.current.style.backgroundColor = "white";
+        sidebarRightImages.current.style.color = "black";
+        sidebarRightVideos.current.style.backgroundColor = "white";
+        sidebarRightVideos.current.style.color = "black";
+      };
+      sidebarRightImages.current.onclick = () => {
+        sidebarRightText.current.style.backgroundColor = "white";
+        sidebarRightText.current.style.color = "black";
+        sidebarRightLinks.current.style.backgroundColor = "white";
+        sidebarRightLinks.current.style.color = "black";
+        sidebarRightImages.current.style.backgroundColor = "#1937be";
+        sidebarRightImages.current.style.color = "white";
+        sidebarRightVideos.current.style.backgroundColor = "white";
+        sidebarRightVideos.current.style.color = "black";
+      };
+      sidebarRightVideos.current.onclick = () => {
+        sidebarRightText.current.style.backgroundColor = "white";
+        sidebarRightText.current.style.color = "black";
+        sidebarRightLinks.current.style.backgroundColor = "white";
+        sidebarRightLinks.current.style.color = "black";
+        sidebarRightImages.current.style.backgroundColor = "white";
+        sidebarRightImages.current.style.color = "black";
+        sidebarRightVideos.current.style.backgroundColor = "#1937be";
+        sidebarRightVideos.current.style.color = "white";
+      };
     }
 
-  }
-
-
-  if(arrowDown.current && downData.current){
-
-    arrowDown.current.onmouseover=()=>{
-      arrowDown.current.style.backgroundColor="#DBDBDB"
+    if (arrowDown.current && downData.current) {
+      arrowDown.current.onmouseover = () => {
+        arrowDown.current.style.backgroundColor = "#DBDBDB";
+      };
+      arrowDown.current.onmouseout = () => {
+        arrowDown.current.style.backgroundColor = "#FFFFFF";
+      };
+      arrowDown.current.onclick = () => {
+        if (downData.current.style.display == "block") {
+          downData.current.style.display = "none";
+        } else {
+          downData.current.style.display = "block";
+        }
+      };
     }
-    arrowDown.current.onmouseout=()=>{
-      arrowDown.current.style.backgroundColor="#FFFFFF"
-    }
-    arrowDown.current.onclick=()=>{
-      if(downData.current.style.display=="flex"){
-        downData.current.style.display="none";
-      }else{
-        downData.current.style.display="flex";
-      }
-    }
-  }
-
-
-},[])
-
+  }, []);
 
   return (
     <div id="child_administrator_matter">
-
-
-
-
-{/* first */}
-{/* first */}
-{/* first */}
-{/* first */}
-{/* first */}
-{/* first */}
-{/* first */}
-{/* first */}
-{/* first */}
-
-
+      {/* first */}
+      {/* first */}
+      {/* first */}
+      {/* first */}
+      {/* first */}
+      {/* first */}
+      {/* first */}
+      {/* first */}
+      {/* first */}
       <div id="child_administrative_matter_headingTop">
-        
         <div id="child_administrative_matter_headingTop_box1"></div>
         <div id="child_administrative_matter_headingTop_box2"></div>
 
-        <div id="child_administrative_matter_headingTop_real">{props.queryTitle}</div>
-        
-        
+        <div id="child_administrative_matter_headingTop_real">
+          {props.queryTitle}
         </div>
-
-{/* //second */}
-{/* //second */}
-{/* //second */}
-{/* //second */}
-{/* //second */}
-{/* //second */}
-{/* //second */}
-
-
-
+      </div>
+      {/* //second */}
+      {/* //second */}
+      {/* //second */}
+      {/* //second */}
+      {/* //second */}
+      {/* //second */}
+      {/* //second */}
       <div id="child_administrative_matter_sideBarLeft">
         <div id="child_administrative_matter_sideBarLeft_organisationHeading">
-Organisation
+          Organisation
         </div>
         <div id="child_administrative_matter_sideBarLeft_organisationName">
-{props.queryOrganisation}
+          {props.queryOrganisation}
         </div>
         <div id="child_administrative_matter_sideBarLeft_departmentHeading">
-Department
+          Department
         </div>
         <div id="child_administrative_matter_sideBarLeft_departmentName">
-{props.queryDepartment}
+          {props.queryDepartment}
         </div>
         <div id="child_administrative_matter_sideBarLeft_dateHeading">
-Recieved At
+          Recieved At
         </div>
         <div id="child_administrative_matter_sideBarLeft_dateName">
-{props.dateSent}
+          {props.dateSent}
         </div>
-
       </div>
-
-
-
-{/* third */}
-{/* third */}
-{/* third */}
-{/* third */}
-{/* third */}
-{/* third */}
-{/* third */}
-{/* third */}
-
-
-
-
-
+      {/* third */}
+      {/* third */}
+      {/* third */}
+      {/* third */}
+      {/* third */}
+      {/* third */}
+      {/* third */}
+      {/* third */}
       <div id="child_administrative_matter_mainData">
-
         {props.queryDescription}
       </div>
-
-
-
       {/* fourth */}
       {/* fourth */}
       {/* fourth */}
@@ -177,107 +141,92 @@ Recieved At
       {/* fourth */}
       {/* fourth */}
       {/* fourth */}
-
       <div id="child_administrative_matter_sideBarRight">
-      <div id="child_administrative_matter_sideBarRight_text" ref={sidebarRightText}>
-TEXT
+        <div
+          id="child_administrative_matter_sideBarRight_text"
+          ref={sidebarRightText}
+        >
+          TEXT
+        </div>
+        <div
+          id="child_administrative_matter_sideBarRight_links"
+          ref={sidebarRightLinks}
+        >
+          LINKS
+        </div>
+        <div
+          id="child_administrative_matter_sideBarRight_images"
+          ref={sidebarRightImages}
+        >
+          IMAGES
+        </div>
+        <div
+          id="child_administrative_matter_sideBarRight_videos"
+          ref={sidebarRightVideos}
+        >
+          VIDEOS
+        </div>
       </div>
-      <div id="child_administrative_matter_sideBarRight_links" ref={sidebarRightLinks}>
-LINKS
+      {/* fifth */}
+      {/* fifth */}
+      {/* fifth */}
+      {/* fifth */}
+      {/* fifth */}
+      {/* fifth */}
+      {/* fifth */}
+      {/* fifth */}
+      {/* fifth */}
+      {/* fifth */}
+      <div id="child_administrator_matter_downBarClicks" ref={arrowDown}>
+        <span
+          id="child_administrator_matter_downBarClicks_icon"
+          class="material-symbols-outlined"
+        >
+          arrow_drop_down
+        </span>
       </div>
-      <div id="child_administrative_matter_sideBarRight_images" ref={sidebarRightImages}>
-IMAGES
+      <div id="child_administrative_matter_BottomHide" ref={downData}>
+        <div id="child_administrative_matter_downBarBottom">
+          {/* first */}
+          <div id="child_administrative_matter_downBarBottom_first">
+            <div id="child_administrative_matter_downBarBottom_first_heading">
+              Comment for querySender
+            </div>
+            <div id="child_administrative_matter_downBarBottom_first_textarea">
+              <textarea id="child_administrative_matter_downBarBottom_first_textareaReal">
+                Enter here
+              </textarea>
+            </div>
+            <div id="child_administrative_matter_downBarBottom_first_submitComment">
+              Send
+            </div>
+          </div>
+          {/* first line */}
+          <div id="child_administrative_matter_downBarBottom_firstLine"></div>
+          {/* second */}
+          <div id="child_administrative_matter_downBarBottom_second">
+            <div id="child_administrative_matter_downBarBottom_second_header">
+              Mark as
+            </div>
+
+            <div id="child_administrative_matter_downBarBottom_second_completed">
+              Completed
+            </div>
+
+            <div id="child_administrative_matter_downBarBottom_second_inProgress">
+              IN-Progress
+            </div>
+            <div id="child_administrative_matter_downBarBottom_second_save">
+              Save
+            </div>
+          </div>
+        </div>
+        <div id="child_administrative_matter_downBottom_delete">
+          Delete This Query
+        </div>
       </div>
-      <div id="child_administrative_matter_sideBarRight_videos" ref={sidebarRightVideos}>
-VIDEOS
-      </div>
-
-      </div>
-
-
-
-
-      {/* fifth */}
-      {/* fifth */}
-      {/* fifth */}
-      {/* fifth */}
-      {/* fifth */}
-      {/* fifth */}
-      {/* fifth */}
-      {/* fifth */}
-      {/* fifth */}
-      {/* fifth */}
-<div id="child_administrator_matter_downBarClicks" ref={arrowDown}>
-<span id="child_administrator_matter_downBarClicks_icon"class="material-symbols-outlined">
-arrow_drop_down
-</span>
-</div>
-
-
-
-
-
-<div id="child_administrative_matter_downBarBottom" ref={downData}>
-
-  {/* first */}
-<div id="child_administrative_matter_downBarBottom_first">
-<div id="child_administrative_matter_downBarBottom_first_heading">
-  Comment for querySender
-</div>
-<div id="child_administrative_matter_downBarBottom_first_textarea">
-<textarea id="child_administrative_matter_downBarBottom_first_textareaReal">Enter here</textarea>
-</div>
-<div id="child_administrative_matter_downBarBottom_first_submitComment">
-  Send
-</div>
-</div>
-
-
-{/* first line */}
-<div id="child_administrative_matter_downBarBottom_firstLine">
-
-</div>
-
-
-
-{/* second */}
-<div id="child_administrative_matter_downBarBottom_second">
-<div id="child_administrative_matter_downBarBottom_second_header">
-  Mark as 
-</div>
-
-<div id="child_administrative_matter_downBarBottom_second_completed">
-  Completed
-</div>
-
-<div id="child_administrative_matter_downBarBottom_second_inProgress">
-  IN-Progress
-</div>
-
-</div>
-
-
-
-{/* second line */}
-<div id="child_administrative_matter_downBarBottom_secondLine">
-
-</div>
-
-
-
-
-
-{/* third */}
-<div id="child_administrative_matter_downBarBottom_third">
-Delete Query
-</div>
-
-      </div>
-
-
-
     </div>
-  )
-}
+  );
+};
 
-export default Child_Administrative_matter
+export default Child_Administrative_matter;
