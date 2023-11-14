70ee084719fc338cff9b76727cc45595fc7ea3de
Gaurav Gautam <gautamgaurav945@gmail.com>
2023-11-15 01:55:43 +0530

    added howTo guide design

--- client/src/Components/My_Queries.jsx
+++ client/src/Components/My_Queries.jsx
@@ -7,27 +7,57 @@ const My_Queries = () => {
 
   window.onscroll = () => {
     const curScroll = window.scrollY;
-    console.log(curScroll, curScroll / 100 - 1);
+    console.log(
+      "window scroll By",
+      curScroll,
+      98 - Math.min(98, curScroll - 961)
+    );
     if (tagLine.current && tagLine_inner.current) {
       // tagLine.current.style.transform = `translate(0px, ${window.scrollY}px)`;
-      if (curScroll >= 679.3333129882812) {
+      if (curScroll >= 981) {
+        tagLine.current.style.transform = `translate(0px, ${
+          window.scrollY - 120
+        }px)`;
+        tagLine.current.style.width = `${98 - Math.min(88, curScroll - 981)}vw`;
+        tagLine.current.style.height = `${
+          98 - Math.min(78, curScroll - 981)
+        }vh`;
+        // tagLine.current.style.borderRadius = "50%";
+        // tagLine.current.style.backgroundColor = "rgba(255, 255, 255, 0.01)";
+      } else if (curScroll >= 782) {
+        tagLine.current.style.marginTop = `782-${Math.min(786, curScroll)}`;
+      } else if (curScroll >= 679.3333129882812) {
         tagLine.current.style.backgroundColor = "black";
-        tagLine.current.style.transform = `translate(0px , 679px) `;
-        tagLine.current.style.width = `calc(-858px + ${1.5 * curScroll}px)`;
+        tagLine.current.style.transform = `translate(0px , ${window.scrollY}px) `;
+        tagLine.current.style.width = `${Math.min(
+          98,
+          Math.min(777, curScroll) - 667.83331298
+        )}vw`;
+        tagLine.current.style.height = `${Math.min(
+          99,
+          Math.min(777, curScroll) - 658.33331298
+        )}vh`;
+        tagLine.current.style.borderRadius = "0%";
+        // tagLine.current.style.marginTop = "-3rem";
       } else if (curScroll >= 490) {
         tagLine.current.style.backgroundColor = "rgba(255, 255, 255, 0.01)";
         // tagLine.current.style.transform = `translate(0px, 0px)`;
+        tagLine.current.style.width = "128px";
+        tagLine.current.style.height = "128px";
         tagLine.current.style.transform = `translate(0px, ${window.scrollY}px)`;
-      } else if (curScroll >= 200) {
+        tagLine.current.style.borderRadius = `50%`;
+      } else if (curScroll >= 270) {
         tagLine.current.style.backgroundColor = "rgba(255, 255, 255, 0.01)";
         tagLine.current.style.transform = `translate(0px, ${window.scrollY}px)`;
 
         tagLine_inner.current.style.visibility = "hidden";
+
         tagLine_inner.current.style.opacity = "0";
         tagLine.current.style.border = "2px solid";
-        tagLine.current.style.borderRadius = `${curScroll - 100}%`;
+        tagLine.current.style.borderRadius = `${curScroll - 270}%`;
         tagLine.current.style.width = `calc(858px - ${1.5 * curScroll}px)`;
       } else {
+        tagLine_inner.current.style.fontSize = "xxx-large";
         tagLine.current.style.backgroundColor = "rgba(255, 255, 255, 0.01)";
         tagLine.current.style.transform = `translate(0px, ${window.scrollY}px)`;
 
@@ -49,7 +79,7 @@ const My_Queries = () => {
     if (para.current) {
       para.current.style.transform = `translate(0px ,${window.scrollY}px)`;
 
-      if (curScroll >= 100) {
+      if (curScroll >= 160) {
         // para.current.style.display   = "none";
         para.current.style.visibility = "hidden";
         para.current.style.opacity = "0";
@@ -87,6 +117,7 @@ const My_Queries = () => {
       </div>
       <div id="my_queries_container_firstSecondGap"></div>
       <div id="my_queries_container_second"></div>
+      <div id="randomSpace"></div>
     </>
   );
 };
